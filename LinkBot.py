e95969eba08379ad652c8c0041fcd568874bebdf
Alexandr <90291297+XandrLord@users.noreply.github.com>
2023-07-13 04:57:44 +0300

    Update LinkBot.py

--- LinkBot.py
+++ LinkBot.py
@@ -8,11 +8,11 @@ TOKEN = '5086990515:AAGbhLwYh6QcZjoAXZUe95KQ5C_ZbfkV9AI'
 bot = telebot.TeleBot(TOKEN)
 
 options = webdriver.ChromeOptions()
-options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.binary_location = os.environ.get("/app/.apt/usr/bin/google-chrome")
 options.add_argument("-headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+driver = webdriver.Chrome(executable_path=os.environ.get("/app/.chromedriver/bin/chromedriver"), chrome_options=options)
 
 status = 0
 
