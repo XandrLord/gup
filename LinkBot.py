6b3745c2f9f61d6ac7fac88b83c7770003e16c1e
Alexandr <90291297+XandrLord@users.noreply.github.com>
2023-07-13 15:50:32 +0300

    Update LinkBot.py

--- LinkBot.py
+++ LinkBot.py
@@ -14,7 +14,7 @@ options.add_argument("-headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 
-chrome_driver_path = '/usr/lib/chromium/chromedriver'
+chrome_driver_path = '/usr/bin/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
 status = 0
