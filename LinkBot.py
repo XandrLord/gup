b00fc5b1140c7c37b444ea98add28e46d18aa3f6
Alexandr <90291297+XandrLord@users.noreply.github.com>
2023-07-13 16:04:20 +0300

    Update LinkBot.py

--- LinkBot.py
+++ LinkBot.py
@@ -14,10 +14,13 @@ options.add_argument("-headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 
-chrome_driver_path = '/usr/bin/chromedriver'
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service, options=options)
+chrome_binary_location = options.binary_location
+print("Chrome binary location:", chrome_binary_location)
 
+driver_binary_location = chrome_driver_path
+print("ChromeDriver binary location:", driver_binary_location)
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 status = 0
 
 @bot.message_handler(commands=['start'])
